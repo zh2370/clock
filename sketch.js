50cfe4ae20db9765d626d8c6d3e75617e1b3d6e6
zh2370 <123714617+zh2370@users.noreply.github.com>
2024-01-10 16:23:31 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+    createCanvas(800,600);
+    noFill();
+  }
+  
+  function draw() {
+    background(255);
+    let hr = hour();
+    let mn = minute();
+    let sc = second();
+  
+    stroke(255, 150, 150, 100);
+    strokeWeight(2);
+    let scSize = map(sc, 0, 60, 0, height);
+    ellipse(width / 2, height / 2, scSize);
+  
+    stroke(150, 255, 150, 100);
+    strokeWeight(5);
+    let mnSize = map(mn, 0, 60, 0, height);
+    ellipse(width / 2, height / 2, mnSize);
+  
+    stroke(150, 150, 255, 100);
+    strokeWeight(15);
+    let hrSize = map(hr % 12, 0, 12, 0, height);
+    ellipse(width / 2, height / 2, hrSize);
+  }
+  

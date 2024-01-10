391770d90ded0fd3cbdbe62a481b494ee307912e
zh2370 <123714617+zh2370@users.noreply.github.com>
2024-01-10 17:24:13 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
+lastMinute = -1;
+
 function setup() {
     createCanvas(800,600);
     noFill();
   }
   
   function draw() {
+    
     background(255);
     let hr = hour();
     let mn = minute();
     let sc = second();
-  
+    
+    if (mn !== lastMinute) {
+        console.log("Minute:", mn);
+        lastMinute = mn;
+    }
+
     stroke(255, 150, 150, 100);
     strokeWeight(2);
     let scSize = map(sc, 0, 60, 0, height);
@@ -20,7 +28,7 @@ function setup() {
     ellipse(width / 2, height / 2, mnSize);
   
     stroke(150, 150, 255, 100);
-    strokeWeight(15);
+    strokeWeight(10);
     let hrSize = map(hr % 12, 0, 12, 0, height);
     ellipse(width / 2, height / 2, hrSize);
   }
